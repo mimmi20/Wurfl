24ed36588a5a37e02e861c4c2bbff16ff827ac7b
Thomas Müller <t_mueller_stolzenhain@yahoo.de>
2013-10-27 20:04:22 +0100

    fixed include

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,8 @@
 <?php
-require_once '../vendor/autoload.php';
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
+chdir(dirname(__DIR__));
+
+require 'vendor/autoload.php';
