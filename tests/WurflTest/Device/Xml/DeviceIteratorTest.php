d9d03450924167a6691ed98ef751ee44e977c769
Thomas Müller <mimmi20@live.de>
2016-03-05 12:40:31 +0100

    remove test

--- tests/WurflTest/Device/Xml/DeviceIteratorTest.php
+++ tests/WurflTest/Device/Xml/DeviceIteratorTest.php
@@ -17,25 +17,4 @@ class DeviceIteratorTest extends \PHPUnit_Framework_TestCase
         $wurflFile = '';
         new DeviceIterator($wurflFile);
     }
-
-    public function testShouldReadTheSpecificAttribute()
-    {
-        $deviceIterator = new DeviceIterator('tests/resources/wurfl-specific-attribute.xml');
-        $devices        = $this->toList($deviceIterator);
-
-        self::assertEquals('foo', $devices[0]->id);
-        self::assertTrue($devices[0]->specific);
-
-        self::assertFalse($devices[1]->specific);
-    }
-
-    private function toList($deviceIterator)
-    {
-        $deviceList = array();
-        foreach ($deviceIterator as $device) {
-            $deviceList[] = $device;
-        }
-
-        return $deviceList;
-    }
 }
